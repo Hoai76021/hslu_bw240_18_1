8b7e771239069cfbb24bf430b988caaadd850b95
peer.kueppers <peer.kueppers@NBFB3KUEPPERS.CAMPUS.fh-ludwigshafen.de>
2018-02-21 15:11:35 +0100

    # WARNING: head commit changed in the meantime
    initial commit

---
+++ src/de/hs_lu/o2s/ueb/ue2/cardgames/TestKartenstapel.java
@@ -0,0 +1,50 @@
+package de.hs_lu.o2s.ueb.ue2.cardgames;
+
+import junit.framework.TestCase;
+
+public class TestKartenstapel extends TestCase {
+
+//	public void testKonstruktoren(){
+//		Kartenstapel myStapel1 = new Kartenstapel();
+//		Kartenstapel myStapel2 =
+//				new Kartenstapel(new Spielkarte("herz", "as"));
+//		Kartenstapel myStapel3 = new Kartenstapel(new Kartenspiel());
+//		Kartenstapel myStapel4 = new Kartenstapel(4);
+//		assertEquals(myStapel1.getKartenAnzahl(), 0);
+//		assertEquals(myStapel2.getKartenAnzahl(), 1);
+//		assertEquals(myStapel3.getKartenAnzahl(), 52);
+//		assertEquals(myStapel4.getKartenAnzahl(), 208);
+//	}
+//	public void testAddMethoden(){
+//		Kartenstapel myStapel1 = new Kartenstapel(new Kartenspiel());
+//		myStapel1.addKartenspiel(new Kartenspiel());
+//		assertEquals(myStapel1.getKartenAnzahl(), 104);
+//		myStapel1.addKartenstapel(new Kartenstapel(new Kartenspiel()));
+//		assertEquals(myStapel1.getKartenAnzahl(), 156);
+//	}
+//	public void testStapelmethoden(){
+//		Kartenstapel myStapel1 = new Kartenstapel();
+//		assertTrue(myStapel1.empty());
+//		Spielkarte myCard1 = new Spielkarte("herz", "as");
+//		Spielkarte myCard2 = new Spielkarte("karo", "koenig");
+//		myStapel1.push(myCard1);
+//		assertFalse(myStapel1.empty());
+//		assertTrue(myStapel1.top() == myCard1);
+//		myStapel1.push(myCard2);
+//		assertTrue(myStapel1.top() == myCard2);
+//		assertTrue(myStapel1.pop() == myCard2);
+//		assertFalse(myStapel1.empty());
+//		assertTrue(myStapel1.pop() == myCard1);
+//		assertTrue(myStapel1.empty());
+//	}
+//	public void testGetPunktwert(){
+//		Kartenstapel myStapel1 = new Kartenstapel(new Kartenspiel());
+//		assertEquals(myStapel1.getPunktwert(), 296);
+//	}
+//	public void testMischen(){
+//		Kartenstapel myStapel1 = new Kartenstapel(new Kartenspiel());
+//		myStapel1.mischen();
+//		System.out.println(myStapel1);
+//	}
+
+}
